e3e01bf02934ed4a22d8e2df5eafb343ddb51c50
simon <ampersand8@gmail.com>
2015-08-02 16:46:12 +0200

    add page for pulp_manager

--- hello.py
+++ hello.py
@@ -1,14 +1,22 @@
 """Cloud Foundry test"""
-from flask import Flask
+from flask import Flask, render_template
 import os
  
 app = Flask(__name__)
  
 port = int(os.getenv("VCAP_APP_PORT"))
- 
+
+
 @app.route('/')
 def hello_world():
-    return 'Hello World! I am running on port ' + str(port)
- 
+    return render_template('index.html')
+
+
+@app.route('/pulp_manager')
+def pulp_manager():
+    return render_template('pulp_manager.html')
+
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=port)
